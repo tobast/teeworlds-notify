4840dfe43a2910ee9bd2501c4c2740c34a62b3c3
Théophile Bastian <contact@tobast.fr>
2016-08-28 16:31:50 +0200

    First commit - join, leave

---
+++ teeworlds-notify.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+
+def onLeave(line, match):
+    return "[TEEWORLDS] Départ de {} :(".format(match.group(2))
+
+
+def onJoin(line, match):
+    return "[TEEWORLDS] Arrivée de {} \o/".format(match.group(3))
+
+ACTIONS = [
+    (re.compile("leave player='([0-9]+):([^']+)'"), onLeave),
+    (re.compile("team_join (. )*player='([0-9]+):([^']+)'"), onJoin),
+    ]
+
+if __name__ == '__main__':
+    for line in sys.stdin:
+        for (regexp, action) in ACTIONS:
+            match = regexp.search(line)
+            if match:
+                print(action(line, match))
+                break
